28c7aed5637fe8abdf0b24dc57a649f951c698d3
曹操 <xuzhao.sgn@gmail.com>
2023-05-04 21:11:24 +0800

    init doc and add tailwindcss

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
+}
+
+// @ts-ignore
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
 })
 
-module.exports = withNextra()
+
+
+
+module.exports = withNextra(nextConfig)
--- theme.config.tsx
+++ theme.config.tsx
@@ -2,7 +2,11 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: 
+    <div className='flex items-center gap-2'>
+      <img src='/image/logo.png' alt='StaFi Protocol' className='w-8'/>
+      <span>StaFi Protocol</span>
+    </div>,
   project: {
     link: 'https://github.com/shuding/nextra-docs-template',
   },
