1d6608d83b73814f1ad4c943f9eca7bb3288912a
曹操 <xuzhao.sgn@gmail.com>
2023-05-05 22:52:37 +0800

    update category

--- theme.config.tsx
+++ theme.config.tsx
@@ -3,9 +3,9 @@ import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
   logo: 
-    <div className='flex items-center gap-2'>
+    <div className='flex items-center gap-3'>
       <img src='/image/logo.png' alt='StaFi Protocol' className='w-8'/>
-      <span>StaFi Protocol</span>
+      <span className='font-bold text-xl'>StaFi Protocol</span>
     </div>,
   project: {
     link: 'https://github.com/shuding/nextra-docs-template',
