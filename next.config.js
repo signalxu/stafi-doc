89e4773b2315519659180e7f0c4cc0b9ac17f376
曹操 <xuzhao.sgn@gmail.com>
2023-05-23 20:54:51 +0800

    add product category

--- next.config.js
+++ next.config.js
@@ -3,15 +3,13 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   trailingSlash: true,
-  images: {
-    unoptimized: true,
-  },
 }
 
 // @ts-ignore
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
+  staticImage: true,
   latex: true,
 })
 
